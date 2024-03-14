106dcd94c4a2e442f3d556f182d7076787431ee5
Leticia Segurasse <146223401+leticiasegurasse@users.noreply.github.com>
2024-03-13 22:15:00 -0300

    Create ContaEmpresarial.java

---
+++ Desafio1/entidades/ContaEmpresarial.java
@@ -0,0 +1,31 @@
+package entidades;
+
+public class ContaEmpresarial extends Conta { // 1 - extends: Indica que ContaEmpresarial está herdando Conta 
+	
+	private double limiteEmprestimo;
+	
+	public ContaEmpresarial() {
+		super();// 2 - Chama o método construtor padrão da classe pai
+	}
+	
+	// 3- Adiciona os mesmos parâmetros do construtor customizado da classe pai 
+	// mais o parametro do construtor customizado da classe filha
+	public ContaEmpresarial(int numero, String titular, double saldo, double limiteEmprestimo) { 
+		super(numero, titular, saldo);              // 4 - Chama o método construtor customizado da classe pai
+		this.limiteEmprestimo = limiteEmprestimo;
+	}
+	
+	public double getLimiteEmprestimo() {
+		return limiteEmprestimo;
+	}
+
+	public void setLimiteEmprestimo(double limiteEmprestimo) {
+		this.limiteEmprestimo = limiteEmprestimo;
+	}
+
+	public void emprestar(double valorEmprestimo) {
+		if (valorEmprestimo <= limiteEmprestimo) {
+			saldo -= valorEmprestimo;
+		}
+	}
+}
