ec950fe15395ad92a0f951338c5dc1424c65630e
Leticia Segurasse <146223401+leticiasegurasse@users.noreply.github.com>
2024-04-07 22:48:17 -0300

    Create Programa.java

---
+++ Estrutura de Dados/Lista Encadeada/Questão 2/Programa.java
@@ -0,0 +1,18 @@
+public class Programa{
+   public static void main(String[] args){
+   Lista lista = new Lista();
+   
+   
+   lista.inserirInicio(10);
+   lista.inserirInicio(20);
+   lista.inserirInicio(30);
+   lista.inserirInicio(40);
+   
+   
+   lista.inserirFinal(0);
+   lista.inserirFinal(1);
+   lista.inserirFinal(2);
+   
+   lista.imprimirReverso();
+   }
+}
