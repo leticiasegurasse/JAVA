4392a74df18592d7ef26a3056c3750494c757f66
Leticia Segurasse <146223401+leticiasegurasse@users.noreply.github.com>
2024-04-08 14:16:56 -0300

    Update Programa.java

--- Estrutura de Dados/Matriz/Questão 1/Programa.java
+++ Estrutura de Dados/Matriz/Questão 1/Programa.java
@@ -5,16 +5,18 @@ public class Programa{
       {4, 5, 6},
       {7, 8, 9}
    };
-   
-   public static void imprimirDiagonal(int[][] matriz){
-      for(int i=0; i< matriz.length; i++){
-         System.out.println("[" + matriz[i][i] + "]");
-      
-      }
-   
-   }
 
+   
+   public static void imprimirSomaDiagonal(int[][] matriz){
+      int soma = 0;
+      for(int i=0; i< matriz.length; i++){
+         soma += matriz[i][i];
+         System.out.println("[" + matriz[i][i] + "]");
+      }
+      System.out.println("Soma: " + soma);
+   }
+   
    public static void main(String[] args){
-      imprimirDiagonal(matriz);
+      imprimirSomaDiagonal(matriz);
    }
 }
